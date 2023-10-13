f0b2f3a6e70ba413f5a42b676869b2e3143339ca
jhonrymat <jhonrymat@gmail.com>
2023-10-13 10:33:21 -0500

    Version 0.1

---
+++ frontend/pages/users.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-row>
+        <v-col>
+            <v-card elevation="7">
+                <v-card class="pa-2" color="#424242" dark>Lista de contactos</v-card>
+            </v-card>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+    export default{
+
+    }
+</script>
+
+<style>
+
+</style>
